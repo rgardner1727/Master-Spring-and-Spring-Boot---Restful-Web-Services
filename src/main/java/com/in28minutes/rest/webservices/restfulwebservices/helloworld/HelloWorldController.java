23b4688654ba110ac98d93512700bdfe37a1878a
Ryan Gardner <rgardner1727@gmail.com>
2024-09-28 19:17:46 -0400

    Used messages.properties to implement internationalization in the application, allowing the use of certain Accept-Language headers

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,11 +1,17 @@
 package com.in28minutes.rest.webservices.restfulwebservices.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private MessageSource messageSource;
 
     @GetMapping("/hello-world")
     public String helloWorld(){
@@ -20,5 +26,10 @@ public class HelloWorldController {
     @GetMapping("/hello-world-bean/path-variable/{name}")
     public HelloWorldBean helloWorldBean(@PathVariable String name){
         return new HelloWorldBean(String.format("Hello %s!", name));
+    }
+
+    @GetMapping("/hello-world-internationalized")
+    public HelloWorldBean helloWorldBeanInternationalized(){
+        return new HelloWorldBean(messageSource.getMessage("good.morning.message", null, "Default Message", LocaleContextHolder.getLocale()));
     }
 }
